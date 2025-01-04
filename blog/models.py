4e4475c1212a6ed1f8721304fbc8e0d9e465e838
Shardul Sisodiya <sisodiyashardul523@gmail.com>
2025-01-05 00:36:32 +0530

    [Feat] - Added a model manager for Post model to filter posts having published status

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,11 @@ from django.conf import settings
 
 # Create your models here.
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -21,6 +26,9 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='blog_posts'
     )
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
